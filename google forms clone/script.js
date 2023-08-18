80209e2bbd8f3106c7267b7efa30ebca1236ea00
yakimax <sv1099912@gmail.com>
2023-08-18 21:13:38 +0530

    add

--- google forms clone/script.js
+++ google forms clone/script.js
@@ -1,3 +1,14 @@
 
 
-let formdata = Document.getElementbyId() ;
+let submitbuttonevent = document.getElementbyId(".button-submit").addEventListner("submit", (e)=>{
+    e.preventDefault() ;
+
+    let userName = document.querySelector("#username") ;
+    let userPass = document.querySelector("#password") ;
+    let userMail = document.querySelector("#email") ;
+    if(userMail.value == "" || userName.value == "" || userPass.value == ""){
+        alert("error") ;
+    }else{
+        console.log(userMail.value,userName.value,userPass.value) ;
+    }
+}) ;
